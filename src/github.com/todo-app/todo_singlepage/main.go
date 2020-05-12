1a01357f64fcf487d281c19c25b2474236f1e66b
@Keerthana511 <keerthanadevadas1997@gmail.com>
2020-05-12 10:02:09 +0530

    modified

--- src/github.com/todo-app/todo_singlepage/main.go
+++ src/github.com/todo-app/todo_singlepage/main.go
@@ -9,9 +9,8 @@ import (
 	"gopkg.in/mgo.v2/bson"
 )
 
-var rnd *renderer.Render
-var todosCollection *mgo.Collection
 var db *mgo.Database
+var sess *mgo.Session
 
 const (
 	Hostname       string = "localhost:27017"
@@ -20,19 +19,22 @@ const (
 	port           string = ":8080"
 )
 
-type Todo struct {
-	ID        bson.ObjectId `bson:"_id,omitempty"`
-	Title     string
-	Completed bool
-}
-type TodoModel struct {
-	ID        bson.ObjectId `bson:"_id,omitempty"`
-	Title     string
-	Completed bool
-}
+type (
+	todoModel struct {
+		ID        bson.ObjectId `bson:"_id,omitempty"`
+		Title     string        `bson:"title"`
+		Completed bool          `bson:"completed"`
+	}
+
+	todo struct {
+		ID        string    `json:"id"`
+		Title     string    `json:"title"`
+		Completed bool      `json:"completed"`
+
+	}
+)
 
 func init() {
-	rnd = renderer.New()
 	sess, err := mgo.Dial(Hostname)
 	if err != nil {
 		panic(err)
@@ -41,32 +43,141 @@ func init() {
 	db = sess.DB(DBname)
 }
 
+func homeHandler(c *gin.Context) {
+	c.HTML(
+		http.StatusOK,
+		"home.tpl",
+		gin.H{
+			"Title": "Home Page",
+		},
+	)
+}
+
 var router *gin.Engine
 
 func main() {
+	defer sess.Close()
 	router = gin.Default()
 	router.LoadHTMLGlob("templates/*")
 	v1 := router.Group("/v1")
 	{
-		router.GET("/", func(c *gin.Context) {
-			c.HTML(
-				http.StatusOK,
-				"home.tpl",
-				gin.H{
-					"title": "Home Page",
-				},
-			)
-		})
+
 		v1.POST("/", createTodo)
-		v1.GET("/{:id}", fetchTodo)
-		v1.GET("/", fetchAllTodo)
+		v1.GET("/", fetchTodos)
 		v1.PUT("/{id}", updateTodo)
 		v1.DELETE("/{id}", deleteTodo)
 	}
 	router.Run()
 }
-func fetchAllTodo(context *gin.Context) {}
-func fetchTodo(context *gin.Context)    {}
-func createTodo(context *gin.Context)   {}
-func updateTodo(context *gin.Context)   {}
-func deleteTodo(context *gin.Context)   {}
+
+func fetchTodos(c *gin.Context) {
+	todos := []todoModel{}
+
+	if err := db.C(collectionName).
+		Find(bson.M{}).
+		All(&todos); err != nil {
+		c.JSON(http.StatusProcessing, gin.H{
+			"message": "Failed to fetch todo",
+			"error":   err,
+		})
+		return
+	}
+
+	todoList := []todo{}
+	for _, t := range todos {
+		todoList = append(todoList, todo{
+			ID:        t.ID.Hex(),
+			Title:     t.Title,
+			Completed: t.Completed,
+		})
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"data": todoList,
+	})
+}
+func createTodo(c *gin.Context) {
+	var t todo
+	c.BindJSON(&t)
+	if t.Title == "" {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"message": "The title field is requried",
+		})
+		return
+	} else {
+		c.JSON(http.StatusOk, t)
+	}
+
+	tm := todoModel{
+		ID:        bson.NewObjectId(),
+		Title:     t.Title,
+		Completed: false,
+	}
+	if err := db.C(collectionName).Insert(&tm); err != nil {
+		c.JSON(http.StatusProcessing, gin.H{
+			"message": "Failed to save todo",
+			"error":   err,
+		})
+		return
+	}
+
+	c.JSON(http.StatusCreated, gin.H{
+		"message": "Todo created successfully",
+		"todo_id": tm.ID.Hex(),
+	})
+}
+func updateTodo(c *gin.Context) {
+	id := bson.ObjectIdHex(c.Param("id"))
+	if !bson.IsObjectIdHex(id) {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"message": "The id is invalid",
+		})
+		return
+	}
+
+	var t todo
+  
+	if t.Title == "" {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"message": "The title field is requried",
+		})
+		return
+	} else {
+		c.JSON(http.StatusOk, t)
+	}
+
+	err := db.C(collectionName).UpdateId(bson.M{"_id": bson.ObjectIdHex(id), "title": t.Title, "completed": t.Completed})
+
+	if err != nil {
+		rnd.JSON(w, http.StatusProcessing, renderer.M{
+			"message": "Failed to update todo",
+			"error":   err,
+		})
+		return
+	} else {
+
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Todo updated successfully",
+		})
+	}
+}
+func deleteTodo(c *gin.Context) {
+	id := bson.ObjectIdHex(c.Param("id"))
+	if !bson.IsObjectIdHex(id) {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"message": "The id is invalid",
+		})
+		return
+	}
+
+	if err := db.C(collectionName).RemoveId(bson.ObjectIdHex(id)); err != nil {
+		c.JSON(http.StatusProcessing, gin.H{
+			"message": "Failed to delete todo",
+			"error":   err,
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Todo deleted successfully",
+	})
+}
